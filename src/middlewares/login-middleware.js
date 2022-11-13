acac099a42ab9afae244cb550b740c7052a77a36
Minhyeong <alsuddl25@icloud.com>
2022-11-13 19:58:12 +0900

    로그인 미들웨어 변수 오타 수정

--- src/middlewares/login-middleware.js
+++ src/middlewares/login-middleware.js
@@ -9,13 +9,14 @@ module.exports = async (req, res, next) => {
     if (!authToken || authType !== 'Bearer') {
       // 토큰이 잘못된 방식으로 전달된 경우...이지만 어쨌든 카카오 인증 성공한 상태이므로 재발급!
       // 에러만 나지 않도록 따로 처리
-      const { email } = jwtService.validateAccessToken(authToken);
+      const { email } = await jwtService.validateAccessToken(authToken);
+      const user = await User.findOne({ email });
       user.accesToken = authToken;
       res.locals.user = user;
       next();
     }
 
-    const { email } = jwtService.validateAccessToken(authToken);
+    const { email } = await jwtService.validateAccessToken(authToken);
 
     const user = await User.findOne({ email });
     // 유효한 토큰일 경우 미리 유저 정보에 넣어서 다음 미들웨어에서 사용할 수 있도록 함
