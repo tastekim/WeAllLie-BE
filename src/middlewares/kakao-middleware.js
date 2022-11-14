0cfe0981db9c4cd42e1d28db30b3673f0f59c05d
Minhyeong <alsuddl25@icloud.com>
2022-11-14 17:39:12 +0900

    테스트

--- src/middlewares/kakao-middleware.js
+++ src/middlewares/kakao-middleware.js
@@ -15,7 +15,7 @@ module.exports = async (req, res, next) => {
     data: qs.stringify({
       grant_type: 'authorization_code',
       client_id: process.env.CLIENT_ID,
-      redirectUri: process.env.CALLBACK_URL_LOCAL,
+      redirectUri: process.env.CALLBACK_URL,
       code: req.query.code,
     }),
   });
