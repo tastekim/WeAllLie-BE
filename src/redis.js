966b2afad097804764cbcc3c85e69a433cb0918e
tastekim <tastekim@kakao.com>
2022-11-16 08:07:21 +0900

    redis 삭제 및 ioredis 설치 및 코드 수정2 #49 comment 참고

--- src/redis.js
+++ src/redis.js
@@ -9,21 +9,3 @@ const redis = new Redis({
 });
 
 module.exports = redis;
-
-// const redis = require('redis');
-// require('dotenv').config();
-//
-// const redisClient = redis.createClient({
-//     url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
-//     legacyMode: true,
-// });
-// redisClient.on('connect', () => {
-//     console.info('Redis connected!');
-// });
-// redisClient.on('error', (err) => {
-//     console.error('Redis Client Error', err);
-// });
-// redisClient.connect().then();
-// const redisCli = redisClient.v4;
-//
-// module.exports = redisCli;
