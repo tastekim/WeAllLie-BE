ace3a3e582ca9a45a5a1e01530cff08ceed6ea31
Minhyeong <alsuddl25@icloud.com>
2022-11-15 11:20:54 +0900

    헤더 및 cors 옵션 추가

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const userRouter = require('./users/user-route');
 const passportConfig = require('./middlewares/passport');
 const cors = require('cors');
 const path = require('path');
+const { ApplicationCostProfiler } = require('aws-sdk');
 
 require('dotenv').config({ path: path.join(__dirname, '/.env') });
 const app = express();
@@ -14,7 +15,11 @@ const http = Server(app);
 
 // middlewares
 passportConfig();
-app.use(cors());
+app.use(cors({ origin: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
