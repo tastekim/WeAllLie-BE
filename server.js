a83286506cf938dafdd571462cabf06c29651a64
Minhyeong <alsuddl25@icloud.com>
2022-11-24 14:38:05 +0900

    로컬에서 HTTP로 동작하도록 코드 변경 #103

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const { http, https } = require('./src/app');
+const { http, app } = require('./src/app');
+const HTTPS = require('https');
+const fs = require('fs');
 require('dotenv').config();
 require('./src/socket');
 require('./src/game/game-socket');
@@ -15,19 +17,23 @@ try {
 
 mongodb();
 
-try {
-    if (process.env.NODE_ENV === 'production') {
-        https.listen(process.env.PORT, () => {
-            console.log(`HTTPS SERVER CONNECTED, PORT :: ${process.env.PORT}}`);
+// 운영 환경일때만 적용
+if (process.env.NODE_ENV == 'production') {
+    try {
+        const option = {
+            ca: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem`),
+            key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem`),
+            cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/cert.pem`),
+        };
+
+        HTTPS.createServer(option, app).listen(3000, () => {
+            console.log('HTTPS 서버가 실행되었습니다. 포트 :: ' + process.env.PORT);
         });
-    } else {
-        http.listen(process.env.PORT, () => {
-            console.log(`HTTP SERVER CONNECTED, PORT :: ${process.env.PORT}}`);
-        });
+    } catch (error) {
+        console.log('HTTPS 서버가 실행되지 않습니다.');
     }
-} catch (e) {
-    console.log(e.message);
+} else {
     http.listen(process.env.PORT, () => {
-        console.log(`connected on HTTP, PORT :: ${process.env.PORT}}`);
+        console.log('HTTP 서버가 실행되었습니다. 포트 :: ' + process.env.PORT);
     });
 }
--- src/socket.js
+++ src/socket.js
@@ -1,16 +1,24 @@
 const socketIo = require('socket.io');
-const { http, https } = require('./app');
+const { http, app } = require('./app');
+const HTTPS = require('https');
+const fs = require('fs');
 const cors = require('cors');
+require('dotenv').config();
 
 let io;
 if (process.env.NODE_ENV === 'production') {
-    io = socketIo(https, cors({ origin: '*' }));
-} else {
-    io = socketIo(http);
+    try {
+        const option = {
+            ca: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem`),
+            key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem`),
+            cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/cert.pem`),
+        };
+        const https = HTTPS.createServer(option, app);
+        io = socketIo(https, cors({ origin: '*' }));
+    } catch (e) {
+        console.log('io가 HTTPS 에서 실행되지 않습니다.');
+    }
 }
-
-io.on('connection', (socket) => {
-    console.log('socket.js connected', socket.id);
-});
+io = socketIo(http);
 
 module.exports = io;
