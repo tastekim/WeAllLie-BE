d169e7f8e53c30387b2c0112fa9cdb4d92d7e3f4
Minhyeong <alsuddl25@icloud.com>
2022-11-30 00:58:01 +0900

    로그인 시 토큰 전달 => 쿠키로 토큰 전달 + 유저 닉네임 바디 전달로 변경 / 인증미들웨어 수정 / 유저 정보 조회 api 추가

--- src/middlewares/login-middleware.js
+++ src/middlewares/login-middleware.js
@@ -7,17 +7,18 @@ module.exports = async (req, res, next) => {
         console.log('------------------------------------');
         console.log('여기는 login-middleware.js');
         // 직접 발급한 토큰이 없다면 다음으로 넘기기 (직접 토큰 발급 받을 수 있도록)
-        const { accesstoken } = req.headers;
-        console.log('로그인 미들웨어 accessToken 확인 ::::::', accesstoken);
-        if (!accesstoken) {
+        const { authorization } = req.headers;
+        const accessToken = (authorization || '').split(' ')[1];
+        console.log('로그인 미들웨어 accessToken 확인 ::::::', accessToken);
+        if (!accessToken) {
             next();
         } else {
             // authToken(카카오에서 받은 토큰)을 헤더의 authorization으로 넘겨주고 있으므로,
             // 직접 발급한 토큰은 accessToken 으로 구분하기
-            const _id = await jwtService.validateAccessToken(accesstoken);
+            const _id = await jwtService.validateAccessToken(accessToken);
             const user = await UserRepo.findOneById(_id);
             console.log('user::::::', user);
-            const userPlayRecord = await UserRepo.getPlayRecord(user, accesstoken);
+            const userPlayRecord = await UserRepo.getPlayRecord(user, accessToken);
             res.status(200).json(userPlayRecord);
         }
     } catch (error) {
