f73fcdb419fe97d0c7658e778dcab4e0a3ae6201
Minhyeong <alsuddl25@icloud.com>
2022-11-30 14:49:49 +0900

    로그인 미들웨어 수정 및 적용, 에러 핸들러 추가

--- src/users/user-route.js
+++ src/users/user-route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const loginMiddleware = require('../middlewares/login-middleware');
 const UserProvider = require('./user-provider');
 
 // 카카오 로그인 1 : 클라이언트에서 인가코드 전달 받음 =>  카카오로 토큰 요청 =>  클라이언트에 카카오 토큰 전달
-router.get('/api/auth/kakao/callback', UserProvider.getKakaoToken);
+router.get('/api/auth/kakao/callback', loginMiddleware, UserProvider.getKakaoToken);
 
 // 카카오 로그인 2 : 토큰으로 카카오에 유저정보 전달하여 클라이언트에 새 토큰 + 유저정보 전달
-router.post('/api/auth/kakao/callback', UserProvider.getKakaoUserInfo);
+router.post('/api/auth/kakao/callback', loginMiddleware, UserProvider.getKakaoUserInfo);
 
 module.exports = router;
