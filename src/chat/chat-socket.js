6854f7e67b8e015846618575fc77057544c4cd6b
Minhyeong <alsuddl25@icloud.com>
2022-12-03 19:36:39 +0900

    룸 퇴장 메세지이벤트 :  정상 종료/ 비정상적 종료 구분

--- src/chat/chat-socket.js
+++ src/chat/chat-socket.js
@@ -20,17 +20,31 @@ chat.on('connection', async (socket) => {
 
     // 룸 입장 메세지
     socket.on('enterRoomMsg', (roomNum, nickname, callback) => {
-        console.log(`${nickname} 방 입장`);
+        console.log(`${nickname} ${roomNum}번 방 입장`);
         const msg = `${nickname} 님이 입장하셨습니다.`;
         socket.to(`/gameRoom${roomNum}`).emit('receiveRoomMsg', { notice: msg });
         callback();
     });
 
-    // 룸 퇴장 메세지
-    socket.on('disconnecting', (roomNum, nickname) => {
-        console.log(`${nickname} 방 퇴장`);
+    // 룸 퇴장 메세지 1 - 나가기 버튼 통해 퇴장
+    socket.on('퇴장메세지이벤트(확인필요)', (roomNum, nickname) => {
+        console.log(`${nickname} ${roomNum}번 방 퇴장`);
         const msg = `${nickname} 님이 퇴장하셨습니다.`;
         socket.to(`/gameRoom${roomNum}`).emit('receiveRoomMsg', { notice: msg });
+    });
+
+    // 룸 퇴장 메세지 2 - 비정상적 연결 해제
+    socket.on('disconnecting', () => {
+        console.log(`disconnecting 이벤트 발생!! ${socket.id}`);
+        console.log('socket.rooms ::', socket.rooms);
+        // socket.rooms.size > 1 이라는 것은 public room 에 입장했던 상태라는 것
+        // 만약 socket.rooms.size 가 1 이라면 굳이 메세지 이벤트를 발생시키지 않아도 된다.
+        if (socket.rooms.size > 1) {
+            const nickname = socket.nickname;
+            console.log(`${nickname} 방 퇴장`);
+            const msg = `${nickname} 님이 퇴장하셨습니다.`;
+            socket.rooms.forEach((room) => socket.to(room).emit('receiveRoomMsg', { notice: msg }));
+        }
     });
 
     // 룸 채팅 (아직 미확정)
@@ -40,5 +54,10 @@ chat.on('connection', async (socket) => {
         console.log(`roomNum::: ${roomNum}`);
         socket.to(`/gameRoom${roomNum}`).emit('receiveRoomMsg', payload);
         callback();
+    });
+
+    socket.on('enterRoomTest', (roomName) => {
+        socket.join(roomName);
+        console.log(`${socket.id} join the room ${roomName}`);
     });
 });
