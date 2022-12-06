306960fc5c6444bceabcb6400617d30c538e852f
kimhsno1 <stone1207@naver.com>
2022-12-06 18:42:21 +0900

    12/6 형석 disconnecting 통합

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const { Server } = require('http');
-// const HTTPS = require('https');
-// const fs = require('fs');
+const HTTPS = require('https');
+const fs = require('fs');
 const logger = require('morgan');
-// const passport = require('passport');
-// const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const userRouter = require('./users/user-route');
 const cors = require('cors');
@@ -13,12 +11,12 @@ require('dotenv').config();
 const app = express();
 const http = Server(app);
 
-// const option = {
-//     ca: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem`),
-//     key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem`),
-//     cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/cert.pem`),
-// };
-// const https = HTTPS.createServer(option, app);
+const option = {
+    ca: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem`),
+    key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem`),
+    cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/cert.pem`),
+};
+const https = HTTPS.createServer(option, app);
 
 // middlewares
 app.use(function (req, res, next) {
@@ -40,22 +38,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-// Node 서버가 프록시 뒤에 있다면 app.use(session({}))을 하기 전에 app.set('trust proxy', 1)을 설정해주는 게 필요하다고 한다.
-// app.use(
-//     session({
-//         name: 'sessionId',
-//         secret: process.env.SESSION_KEY,
-//         resave: false,
-//         saveUninitialized: true,
-//         cookie: {
-//             secure: false,
-//             httpOnly: true,
-//             maxAge: 60 * 60 * 24,
-//         },
-//     })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
 app.use('/', userRouter);
 
-module.exports = { http /*, https*/ };
+module.exports = { http, https };
