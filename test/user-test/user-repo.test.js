65e9cefde429c54f66e949b3fc09601aef29f03c
Minhyeong <alsuddl25@icloud.com>
2022-12-06 23:39:12 +0900

    테스트 코드 작성할 파일 생성

---
+++ test/user-test/user-repo.test.js
@@ -0,0 +1,11 @@
+describe('getNewUser method', () => {});
+describe('getPlayRecord method', () => {});
+
+describe('Product Controller Create', () => {
+    test('should have a createProduct function', () => {
+        expect(typeof productController.createProduct).toBe('function');
+    });
+    test('', () => {});
+    test('', () => {});
+    test('', () => {});
+});
