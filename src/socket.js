3be08e4a2fb7c680531c009f0549a7c3cb42ecfb
tastekim <tastekim@kakao.com>
2022-12-13 15:58:47 +0900

    주석 제거

--- src/socket.js
+++ src/socket.js
@@ -1,27 +1,26 @@
 const socketIo = require('socket.io');
 const redis = require('./redis');
 const { http, https } = require('./app');
-//const http = require('./app');
 const cors = require('cors');
 const RoomProvider = require('./rooms/room-provider');
 require('dotenv').config();
 
 let io;
 
-// if (process.env.NODE_ENV == 'production') {
-//     try {
-//         io = socketIo(https, cors({ origin: '*' }));
-//         console.log('io : HTTPS 로 서버가 실행되었습니다. 포트 :: ' + process.env.PORT);
-//     } catch (e) {
-//         console.log('io : HTTPS로 서버가 실행되지 않습니다.');
-//         console.log(e);
-//         io = socketIo(http);
-//         console.log('io : HTTP 로 서버가 실행되었습니다. 포트 :: ' + process.env.PORT);
-//     }
-// } else {
-//     io = socketIo(http);
-//     console.log('io : HTTP 로 서버가 실행되었습니다. 포트 :: ' + process.env.PORT);
-// }
+if (process.env.NODE_ENV == 'production') {
+    try {
+        io = socketIo(https, cors({ origin: '*' }));
+        console.log('io : HTTPS 로 서버가 실행되었습니다. 포트 :: ' + process.env.PORT);
+    } catch (e) {
+        console.log('io : HTTPS로 서버가 실행되지 않습니다.');
+        console.log(e);
+        io = socketIo(http);
+        console.log('io : HTTP 로 서버가 실행되었습니다. 포트 :: ' + process.env.PORT);
+    }
+} else {
+    io = socketIo(http);
+    console.log('io : HTTP 로 서버가 실행되었습니다. 포트 :: ' + process.env.PORT);
+}
 
 io = socketIo(http);
 console.log('io : HTTP 로 서버가 실행되었습니다. 포트 :: ' + process.env.PORT);
