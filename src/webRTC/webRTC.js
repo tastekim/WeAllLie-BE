853424ff441279616cc8a643ddcabf8611ff84bd
tastekim <tastekim@kakao.com>
2022-12-16 01:13:17 +0900

    불필요한 라인 제거 및 주석 제거

--- src/webRTC/webRTC.js
+++ src/webRTC/webRTC.js
@@ -8,13 +8,11 @@ const openvidu = new OpenVidu(process.env.OPENVIDU_URL, process.env.OPENVIDU_SEC
 console.warn('Application server connecting to OpenVidu at ' + process.env.OPENVIDU_URL);
 
 router.post('/openvidu/api/sessions', async (req, res) => {
-    console.log('session 연결~~');
     const session = await openvidu.createSession(req.body);
     res.send(session.sessionId);
 });
 
 router.post('/openvidu/api/sessions/:sessionId/connections', async (req, res) => {
-    console.log('그 다음 연결~~');
     const session = openvidu.activeSessions.find((s) => s.sessionId === req.params.sessionId);
     if (!session) {
         res.status(404).send();
